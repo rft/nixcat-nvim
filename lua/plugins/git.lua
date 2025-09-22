16fa8b5db09fe63a5466c984e39a63ed0fa2ef18
rft <nano@nomolabs.net>
2025-09-22 23:23:37 +0000

    added keybinds for hunk stuff in gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,4 +9,13 @@ return {
       require("neogit").setup({})
     end,
   },
+
+  -- Git hunk operations
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "[G]it [r]evert hunk" },
+      { "<leader>gR", function() require("gitsigns").stage_hunk() end, desc = "[G]it [R]edo (stage) hunk" },
+    },
+  },
 }
