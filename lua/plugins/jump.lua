6f9e9cb221a0fd7ac21cf06dadac0af6c8b7bed4
rft <nano@nomolabs.net>
2025-09-22 01:38:26 +0000

    added treesitter jump to node with flash

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -4,8 +4,67 @@ return {
     "folke/flash.nvim",
     keys = {
       { "<leader>jw", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Jump to word" },
+      { "<leader>jt", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Jump to treesitter node" },
+      { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote flash" },
+      { "<leader>jR", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter search" },
       { "<leader>jl", "<cmd>lua vim.ui.input({ prompt = 'Go to line: ' }, function(input) if input then vim.cmd('normal! ' .. input .. 'G') end end)<cr>", desc = "Jump to line" },
     },
+    config = function()
+      require("flash").setup({
+        -- Labels to use for flash jumps
+        labels = "asdfghjklqwertyuiopzxcvbnm",
+        search = {
+          -- Show search in command line
+          mode = "exact",
+          -- Maximum number of search results
+          max_length = false,
+        },
+        jump = {
+          -- Save location in jumplist
+          jumplist = true,
+          -- Jump position
+          pos = "start",
+          -- Clear highlight after jump
+          history = false,
+          register = false,
+        },
+        label = {
+          -- Allow uppercase labels
+          uppercase = true,
+          -- Show labels after pattern
+          after = true,
+          before = true,
+          -- Label style
+          style = "overlay",
+        },
+        highlight = {
+          -- Highlight settings
+          backdrop = true,
+          matches = true,
+          priority = 5000,
+        },
+        -- Treesitter mode configuration
+        modes = {
+          search = {
+            enabled = true,
+          },
+          char = {
+            enabled = true,
+            jump_labels = true,
+          },
+          treesitter = {
+            labels = "abcdefghijklmnopqrstuvwxyz",
+            jump = { pos = "range" },
+            search = { incremental = false },
+            label = { before = true, after = true, style = "inline" },
+            highlight = {
+              backdrop = false,
+              matches = false,
+            },
+          },
+        },
+      })
+    end,
   },
   
   -- Jump to changes
