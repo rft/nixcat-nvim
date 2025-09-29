81b6d2ed9b62de6052121ede108f937fcd54a41c
rft <nano@nomolabs.net>
2025-09-29 02:29:02 +0000

    changed some neo tree behavior

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,7 +4,7 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   -- NOTE: nixCats: return true only if category is enabled, else false
-  enabled = require('nixCatsUtils').enableForCategory("kickstart-neo-tree"),
+  enabled = require('nixCatsUtils').enableForCategory 'kickstart-neo-tree',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
@@ -16,10 +16,52 @@ return {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
   opts = {
+    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    popup_border_style = 'rounded',
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
       window = {
+        position = 'left',
+        width = 30,
         mappings = {
           ['\\'] = 'close_window',
+        },
+      },
+      follow_current_file = {
+        enabled = false, -- This will find and focus the file in the active buffer every time
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      group_empty_dirs = false, -- when true, empty folders will be grouped together
+      hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+      use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+    },
+    buffers = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      group_empty_dirs = true, -- when true, empty folders will be grouped together
+      show_unloaded = true,
+      window = {
+        mappings = {
+          ['bd'] = 'buffer_delete',
+          ['<bs>'] = 'navigate_up',
+          ['.'] = 'set_root',
+        },
+      },
+    },
+    git_status = {
+      window = {
+        position = 'float',
+        mappings = {
+          ['A'] = 'git_add_all',
+          ['gu'] = 'git_unstage_file',
+          ['ga'] = 'git_add_file',
+          ['gr'] = 'git_revert_file',
+          ['gc'] = 'git_commit',
+          ['gp'] = 'git_push',
+          ['gg'] = 'git_commit_and_push',
         },
       },
     },
