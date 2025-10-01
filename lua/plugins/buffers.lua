156d1af3b91e3713d70764743d0a6a82b271c914
rft <nano@nomolabs.net>
2025-10-01 22:06:59 +0000

    changed buffer close behavior

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -7,7 +7,24 @@ return {
       { "<leader>bB", "<cmd>Telescope buffers<cr>", desc = "Open all buffer search" },
       { "<leader>,", "<cmd>Telescope buffers cwd_only=true<cr>", desc = "Open buffer search (workspace)" },
       { "<leader><S-,>", "<cmd>Telescope buffers<cr>", desc = "Open all buffer search" },
-      { "<leader>bc", "<cmd>bdelete<cr>", desc = "Close buffer" },
+      { "<leader>bc", function()
+        local current = vim.api.nvim_get_current_buf()
+        local listed = vim.tbl_filter(function(buf)
+          return vim.api.nvim_buf_is_valid(buf)
+            and vim.bo[buf].buflisted
+            and vim.api.nvim_buf_is_loaded(buf)
+        end, vim.api.nvim_list_bufs())
+
+        if #listed > 1 then
+          vim.cmd 'bnext'
+        else
+          vim.cmd 'enew'
+        end
+
+        if vim.api.nvim_buf_is_valid(current) then
+          vim.cmd('bdelete ' .. current)
+        end
+      end, desc = "Close buffer" },
       { "<leader>bs", "<cmd>enew<cr>", desc = "Scratch buffer" },
       { "<leader>bu", "<cmd>e #<cr>", desc = "Reopen last buffer" },
     },
