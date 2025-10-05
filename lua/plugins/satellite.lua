e27b6f8ff89fb04c6af3ebdbd076778c1e989ded
rft <nano@nomolabs.net>
2025-10-05 06:43:03 +0000

    ignored a certain error on buffer close that's technically unchanged

--- lua/plugins/satellite.lua
+++ lua/plugins/satellite.lua
@@ -3,6 +3,29 @@ return {
     'lewis6991/satellite.nvim',
     event = { 'BufReadPost', 'BufNewFile' },
     config = function()
+      local util = require('satellite.util')
+      local original_noautocmd = util.noautocmd
+
+      util.noautocmd = function(fn)
+        return original_noautocmd(function(...)
+          if vim.in_fast_event() then
+            return
+          end
+
+          local results = { pcall(fn, ...) }
+          local ok = table.remove(results, 1)
+          if not ok then
+            local err = results[1]
+            if type(err) == 'string' and err:match('E565: Not allowed to change text or change window') then
+              return
+            end
+            error(err)
+          end
+
+          return unpack(results)
+        end)
+      end
+
       require('satellite').setup {
         current_only = false,
         winblend = 20,
