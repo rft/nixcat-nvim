2ee0383f42510675893e74ca28c856f3d1e2b4be
rft <nano@nomolabs.net>
2025-10-10 03:37:21 +0000

    changed keybinds for arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -13,35 +13,35 @@ return {
     },
     opts = {
       show_icons = true,
-      leader_key = '<leader>al',
+      leader_key = '<leader>wm',
     },
     config = function(_, opts)
       local arrow = require 'arrow'
       local persist = require 'arrow.persist'
       arrow.setup(opts or {})
 
-      vim.keymap.set('n', '<leader>aa', function()
+      vim.keymap.set('n', '<leader>wt', function()
         persist.toggle()
-      end, { desc = '[A]rrow toggle current file' })
+      end, { desc = '[W]orkspace marks toggle' })
 
-      vim.keymap.set('n', '<leader>an', function()
+      vim.keymap.set('n', '<leader>wn', function()
         persist.next()
-      end, { desc = '[A]rrow [N]ext mark' })
+      end, { desc = '[W]orkspace marks next' })
 
-      vim.keymap.set('n', '<leader>ap', function()
+      vim.keymap.set('n', '<leader>wp', function()
         persist.previous()
-      end, { desc = '[A]rrow [P]revious mark' })
+      end, { desc = '[W]orkspace marks previous' })
 
       local ui = require 'arrow.ui'
-      vim.keymap.set('n', '<leader>al', function()
+      vim.keymap.set('n', '<leader>wm', function()
         ui.openMenu()
-      end, { desc = '[A]rrow [L]ist marks' })
+      end, { desc = '[W]orkspace marks menu' })
 
       for i = 1, 9 do
-        local key = string.format('<leader>a%s', i)
+        local key = string.format('<leader>w%s', i)
         vim.keymap.set('n', key, function()
           persist.go_to(i)
-        end, { desc = string.format('[A]rrow file %d', i) })
+        end, { desc = string.format('[W]orkspace mark %d', i) })
       end
     end,
   },
